54bf378e92f3095674d89ae339cb2779b3e7ac9d
Kevin Skompinski <kevons5252@gmail.com>
2016-08-15 21:50:11 -0400

    Initial Commit

---
+++ Sounds/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sounds
+//
+//  Created by Kevin on 8/15/16.
+//  Copyright © 2016 Kevin Skompinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sounds/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Sounds
+//
+//  Created by Kevin on 8/15/16.
+//  Copyright © 2016 Kevin Skompinski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
